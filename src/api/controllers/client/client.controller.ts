5ed6320e55b477dc2ad73b9e95894e7b5cae5235
Higo Alexandre <higoalexandreb@gmail.com>
2019-12-05 07:00:09 -0300

    Update

--- src/api/controllers/client/client.controller.ts
+++ src/api/controllers/client/client.controller.ts
@@ -9,27 +9,27 @@ export class ClientController {
 
     constructor(private readonly service: ClientService){}
 
-    @Get('user/:idUser/client/:idClient')
+    @Get(':idUser/client/:idClient')
     async findById(@Param('idUser') idUser, @Param('idClient') idClient){
         return await this.service.findById(idUser, idClient);
     }
 
-    @Get('user/:idUser/client')
+    @Get(':idUser/client')
     async findByUser(@Param('idUser') idUser){
       return await this.service.findByUser(idUser);
     }
 
-    @Post('user/:idUser/client')
+    @Post(':idUser/client')
     async save(@Param('idUser') idUser, @Body() client: ClientCreate){
       return await this.service.save(idUser, client);
     }
 
-    @Put('user/:idUser/client/:idClient')
+    @Put(':idUser/client/:idClient')
     async update(@Param('idUser') idUser, @Param('idClient') idClient){
       return await this.service.delete(idUser, idClient);
     }
 
-    @Delete('user/:idUser/client/:idClient')
+    @Delete(':idUser/client/:idClient')
     async delete(@Param('idUser') idUser, @Param('idClient') idClient){
       return await this.service.delete(idUser, idClient);
     }
