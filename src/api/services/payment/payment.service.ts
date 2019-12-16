fa7b9c9d03e9675c971bc8a57da621e41bcc8556
Higo Alexandre <higoalexandreb@gmail.com>
2019-12-16 07:07:53 -0300

    Update

--- src/api/services/payment/payment.service.ts
+++ src/api/services/payment/payment.service.ts
@@ -1,4 +1,44 @@
 import { Injectable } from '@nestjs/common';
+import { Payment } from 'src/api/entities/payment.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class PaymentService {}
+export class PaymentService {
+
+    constructor(@InjectRepository(Payment) private readonly repository: Repository<Payment>){}
+
+    async findById(user, paymentId: number): Promise<Payment>{
+        let payment = await this.repository.findOne(paymentId);
+        if(payment.user == user){
+            return payment;
+        }
+    }
+
+    async findAll(user): Promise<Payment[]>{
+        let products = await this.repository.find({
+            where: [{user: user}]
+        });
+        return products;
+    }
+
+    async save(user, payment: Payment): Promise<Payment>{
+        payment.user = user;
+        return this.repository.save(payment);
+    }
+
+    async update(user, payment: Payment, paymentId: number): Promise<Payment>{
+        if(payment.id == paymentId){
+            payment.user = user;
+            return await this.repository.save(payment);
+        }
+    }
+    
+    async delete(user, paymentId: number){
+        let product = await this.repository.findOne(paymentId);
+        if(product.id == paymentId && product.user == user){
+            this.repository.delete({id: paymentId})
+        }
+    }
+
+}
--- src/api/services/sale/sale.service.ts
+++ src/api/services/sale/sale.service.ts
@@ -1,4 +1,44 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Sale } from 'src/api/entities/sale.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class SaleService {}
+export class SaleService {
+
+    constructor(@InjectRepository(Sale) private readonly repository: Repository<Sale>){}
+
+    async findById(user, saleId: number): Promise<Sale>{
+        let payment = await this.repository.findOne(saleId);
+        if(payment.user == user){
+            return payment;
+        }
+    }
+
+    async findAll(user): Promise<Sale[]>{
+        let products = await this.repository.find({
+            where: [{user: user}]
+        });
+        return products;
+    }
+
+    async save(user, sale: Sale): Promise<Sale>{
+        sale.user = user;
+        return this.repository.save(sale);
+    }
+
+    async update(user, sale: Sale, saleId: number): Promise<Sale>{
+        if(sale.id == saleId){
+            sale.user = user;
+            return await this.repository.save(sale);
+        }
+    }
+    
+    async delete(user, saleId: number){
+        let product = await this.repository.findOne(saleId);
+        if(product.id == saleId && product.user == user){
+            this.repository.delete({id: saleId})
+        }
+    }
+
+}
